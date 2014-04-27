0aa6c19cf0de7a20c8ed2ecef7b53dc21fc3bc1c
Stian Eikeland <stian@eikeland.se>
2014-04-27 23:44:12 +0200

    goto line

--- user.el
+++ user.el
@@ -126,3 +126,13 @@
 (global-set-key (kbd "<C-return>") 'open-line-below)
 (global-set-key (kbd "<C-S-return>") 'open-line-above)
 (global-set-key (kbd "<M-return>") 'new-line-dwim)
+
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (call-interactively 'goto-line))
+    (linum-mode -1)))
