f2e1a38bba5c6fe7819f029a77ffc6c8ab3bd0b0
Stian Eikeland <stian@eikeland.se>
2014-04-29 11:24:14 +0200

    disable project explorer (too slow..)

--- se-project.el
+++ se-project.el
@@ -4,7 +4,12 @@
 (projectile-global-mode)
 (global-set-key (kbd "C-c C-f") 'projectile-find-file)
 
-(package-require 'project-explorer)
-(global-set-key (kbd "C-x p") 'project-explorer-open)
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
+
+;; Disable for now, as it's realllllly slooooow on laaaarge proooojects...
+;; (package-require 'project-explorer)
+;; (global-set-key (kbd "C-x p") 'project-explorer-open)
+;; (require 'project-explorer)
+;; (setq pe/omit-regex (concat pe/omit-regex "\\|^node_modules$"))
 
 (provide 'se-project)
