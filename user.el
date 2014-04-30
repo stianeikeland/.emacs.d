c7ca9cf8965a560a9219b395d3b1b20e88498471
Stian Eikeland <stian@eikeland.se>
2014-04-30 23:32:41 +0200

    diminish all the modes!

--- user.el
+++ user.el
@@ -115,6 +115,10 @@
 (eval-after-load "eldoc" '(diminish 'eldoc-mode))
 (eval-after-load "paredit" '(diminish 'paredit-mode))
 (eval-after-load "projectile" '(diminish 'projectile-mode))
+(eval-after-load "elisp-slime-nav" '(diminish 'elisp-slime-nav-mode))
+(eval-after-load "magit-auto-revert-mode" '(diminish 'magit-auto-revert-mode))
+(eval-after-load "yasnippet" '(diminish 'yas-minor-mode))
+
 
 (defmacro rename-modeline (package-name mode new-name)
   `(eval-after-load ,package-name
