86b73cfd009c6a2a8337a41cccb08308a9fdc718
Stian Eikeland <stian@eikeland.se>
2014-05-20 11:22:02 +0200

    highlight background color green

--- init.el
+++ init.el
@@ -23,4 +23,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight ((t (:background "dark green"))))
  '(linum ((t (:foreground "gray53" :inherit (shadow default)))) t))
