95aae3e5d22b31ca8d3bf8c753945a4a842f92c0
Stian Eikeland <stian@eikeland.se>
2014-07-03 10:21:13 +0200

    10 sec deft auto save interval

--- se-deft.el
+++ se-deft.el
@@ -5,6 +5,7 @@
 (setq deft-directory "~/Dropbox/Notes")
 (setq deft-text-mode 'markdown-mode)
 (setq deft-use-filename-as-title t)
+(setq deft-auto-save-interval 10.0)
 
 (global-set-key [f8] 'deft)
 
