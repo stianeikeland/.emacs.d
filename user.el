8469872e60b57a7876c112dc95356bf8512b5ee4
Stian Eikeland <stian@eikeland.se>
2014-07-11 12:54:37 +0200

    prevent annoying popups!

--- user.el
+++ user.el
@@ -250,6 +250,15 @@
 
 ;;(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
 
+;; Prevent f*(#$ annoying popup windows..
+(defadvice yes-or-no-p (around prevent-dialog activate)
+  "Prevent yes-or-no-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+(defadvice y-or-n-p (around prevent-dialog-yorn activate)
+  "Prevent y-or-n-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
 
 
 (provide 'user)
