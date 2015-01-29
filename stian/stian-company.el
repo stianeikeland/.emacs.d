6d7c2dcb20732cab733b8e5f17e3a83a8664f770
Stian Eikeland <stian@eikeland.se>
2015-01-29 21:13:07 +0100

    use new company-quickhelp for popup docs

--- stian/stian-company.el
+++ stian/stian-company.el
@@ -1,6 +1,7 @@
 ;;; stian-company -- company mode completions
 
 (package-require 'company)
+(package-require 'company-quickhelp)
 
 (setq company-idle-delay 0.5)
 (setq company-tooltip-limit 10)
@@ -8,13 +9,14 @@
 
 ;; invert the navigation direction if the the completion popup-isearch-match
 ;; is displayed on top (happens near the bottom of windows)
-(setq company-tooltip-flip-when-above t)
+;; (setq company-tooltip-flip-when-above t)
 
 (package-require 'company-tern)
 
 (add-hook 'after-init-hook
           (lambda ()
             (global-company-mode)
+            (company-quickhelp-mode 1)
             (add-to-list 'company-backends 'company-tern)))
 
 (provide 'stian-company)
