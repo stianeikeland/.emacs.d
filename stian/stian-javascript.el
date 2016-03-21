4fff11fd9df5c85cf6116c37a58a75b65f95ef62
Stian Eikeland <stian@eikeland.se>
2016-03-21 13:25:37 +0100

    Use js2-jsx-mode for .jsx files

--- stian/stian-javascript.el
+++ stian/stian-javascript.el
@@ -4,6 +4,7 @@
 
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
 
 (require 'js2-mode)
