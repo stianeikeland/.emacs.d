b77954ac3d3a06b9bffd84bc7989af4b118e7154
Stian Eikeland <stian@eikeland.se>
2016-08-01 11:12:01 +0200

    web-mode for jsx

--- stian/stian-web.el
+++ stian/stian-web.el
@@ -28,5 +28,34 @@
             (setq web-mode-css-indent-offset 2)
             (setq web-mode-code-indent-offset 2)))
 
+(add-hook 'web-mode-hook 'smartparens-mode)
+
+(package-require 'company-web)
+
+;; (defun my-web-mode-hook ()
+;;   "Hook for `web-mode'."
+;;     (set (make-local-variable 'company-backends)
+;;          '(company-tern company-web-html company-yasnippet company-files)))
+
+;; (add-hook 'web-mode-hook 'my-web-mode-hook)
+
+;; ;; Enable JavaScript completion between <script>...</script> etc.
+;; (defadvice company-tern (before web-mode-set-up-ac-sources activate)
+;;   "Set `tern-mode' based on current language before running company-tern."
+;;   (message "advice")
+;;   (if (equal major-mode 'web-mode)
+;;       (let ((web-mode-cur-language
+;;              (web-mode-language-at-pos)))
+;;         (if (or (string= web-mode-cur-language "javascript")
+;;                 (string= web-mode-cur-language "jsx")
+;;                 )
+;;             (unless tern-mode (tern-mode))
+;;           (if tern-mode (tern-mode -1))))))
+
+
+(add-hook 'web-mode-hook (lambda ()
+                          (set (make-local-variable 'company-backends) '(company-web-html))
+                          (company-mode t)))
+
 
 (provide 'stian-web)
